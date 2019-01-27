9b9e5d39db87049d80a29edc0c93498675849bb8
Денис <denedavydov@gmail.com>
2019-01-27 18:24:48 +0300

    шаблон вывода шапки на странице пользователя

---
+++ templates/header.php
@@ -0,0 +1,19 @@
+<div class="col-md-1 col-xs-12 col-sm-6">
+	<p class="text-center">
+		<img src="img/logo.jpg" alt="логотип" width="100" />
+	</p>
+</div>
+
+<div class="col-md-5 col-xs-12 col-sm-6">
+	<h1 class="text-primary text-center" style="text-shadow: 2px 2px 5px rgba(0,0,0,0.3);">Электронная Приёмная</h1>
+</div>
+
+<div class="col-md-5 text-center col-xs-12 col-sm-6">
+	<p class="text-warning" style="margin-top: 20px;">ГБОУ средняя общеобразовательная школа № 416<br/> Петродворцового района Санкт-Петербурга</p>
+</div>
+
+<div class="col-md-1 col-xs-12 col-sm-6">
+	<form action="" method="POST">
+		<button type="submit" class="btn btn-danger btn-block" name="exit" style="margin-top: 20px;"><span class=""></span> выход</button>
+	</form>
+</div>
