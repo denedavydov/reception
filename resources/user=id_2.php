fdbfc0baa8bf7839ebd733d8f43c41dee160f456
Денис <denedavydov@gmail.com>
2019-01-27 18:25:21 +0300

    добавлен вывод шаблона header.php

--- resources/user=id_2.php
+++ resources/user=id_2.php
@@ -16,26 +16,13 @@
 
 	<div class="container">
 		<div class="row">
-			<div class="col-md-1 col-xs-12 col-sm-6">
-				<p class="text-center">
-					<img src="/img/logo.jpg" alt="логотип" width="100" />
-				</p>
-			</div>
+			
+			<!--шапка страницы-->
+			<?php
+				include('../templates/header.php');
+			?>	
 
-			<div class="col-md-5 col-xs-12 col-sm-6">
-				<h1 class="text-primary text-center" style="text-shadow: 2px 2px 5px rgba(0,0,0,0.3);">Электронная Приёмная</h1>
-			</div>
-
-			<div class="col-md-5 text-center col-xs-12 col-sm-6">
-				<p class="text-warning" style="margin-top: 20px;">ГБОУ средняя общеобразовательная школа № 416<br/> Петродворцового района Санкт-Петербурга</p>
-			</div>
-
-			<div class="col-md-1 col-xs-12 col-sm-6">
-				<form action="" method="POST">
-					<button type="submit" class="btn btn-danger btn-block" name="exit" style="margin-top: 20px;"><span class=""></span> выход</button>
-				</form>
-			</div>
-
+			<!--вывод меню-->
 			<div class="col-xs-12">
 				<?php
 					include('../templates/menu.php');
