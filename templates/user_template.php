e832b9beb46c9e80f60c171fa3af397726583789
Денис <denedavydov@gmail.com>
2019-01-27 18:25:59 +0300

    шаблон страницы пользователя

---
+++ templates/user_template.php
@@ -0,0 +1,50 @@
+<?php
+	include('../templates/session_start.php');
+?>
+<?php
+	include('../templates/session_close.php');
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<base href="/reception/" /><!--абсолютный путь-->
+	<?php
+		include('../templates/head.php');
+	?>
+</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+			
+			<!--шапка страницы-->
+			<?php
+				include('../templates/header.php');
+			?>	
+
+			<!--вывод меню-->
+			<div class="col-xs-12">
+				<?php
+					include('../templates/menu.php');
+				?>
+			</div>
+
+			<div class="col-xs-12">
+				<h2 class="text-primary">Записи</h2>
+			</div>
+
+			<div class="col-xs-12">
+				<h2 class="text-primary">Обращения</h2>
+			</div>
+
+			<div class="col-xs-12">
+				<h2 class="text-primary">Документы ШРР</h2>
+			</div>
+
+
+	
+		</div>
+	</div>
+
+</body>
+</html>
