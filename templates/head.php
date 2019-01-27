3fecde3411372ebc7b54e5847b0bcfb41803bc38
Денис <denedavydov@gmail.com>
2019-01-27 14:23:49 +0300

    преобразовано в чистый html

--- templates/head.php
+++ templates/head.php
@@ -1,5 +1,3 @@
-<?php
-	echo '
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
@@ -13,5 +11,3 @@
 	<!--<script language=JavaScript>
          document.oncontextmenu=new Function("return false;")
     </script>-->
-	';
-?>
