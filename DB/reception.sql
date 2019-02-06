25e8b4e557787204c369f7258b38c4c065fe2879
denedavydov <denedavydov@gmail.com>
2019-02-06 19:48:47 +0300

    новый дамп

--- DB/reception.sql
+++ DB/reception.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost:3306
--- Время создания: Фев 04 2019 г., 10:09
--- Версия сервера: 5.7.25-0ubuntu0.18.04.2
--- Версия PHP: 5.6.40-1+ubuntu18.04.1+deb.sury.org+1
+-- Время создания: Фев 06 2019 г., 19:44
+-- Версия сервера: 5.7.24-0ubuntu0.18.04.1
+-- Версия PHP: 5.6.39-1+ubuntu18.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -66,19 +66,14 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `surname`, `login`, `passport`, `status`, `password`) VALUES
-(1, 'Давыдов', 'Денис', 'denedavydov@gmail.com', '12121212', 'Родитель обучающегося', '12345678'),
+(1, 'Давыдов', 'Денис', 'denedavydov@gmail.com', '1234545454', 'Родитель обучающегося', '12345678'),
 (2, 'Иванов', 'Петр', 'denedavydov@yandex.ru', '7878787878', 'Родитель обучающегося', 'qwerty'),
-(3, 'Петров', 'Сергей Иванович', 'davydov@school416spb.ru', '4545454545', 'Законный представитель обучающегося', '123456');
+(3, 'Петров', 'Сергей Иванович', 'davydov@school416spb.ru', '4545454545', 'Законный представитель обучающегося', '123456'),
+(4, 'admin', '', 'admin@gmail.com', '', '', 'admin1');
 
 --
 -- Индексы сохранённых таблиц
 --
-
---
--- Индексы таблицы `appeals`
---
-ALTER TABLE `appeals`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `users`
@@ -91,15 +86,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT для таблицы `appeals`
---
-ALTER TABLE `appeals`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
