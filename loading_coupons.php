7def99d12f423c62646b4dca0e75e1f54808dbaa
denedavydov <denedavydov@gmail.com>
2019-03-01 20:43:01 +0300

    Исправление ошибок

--- loading_coupons.php
+++ loading_coupons.php
@@ -8,30 +8,28 @@
 	$date = date("d.m.Y");
 	$day = date("w", $date);
 	
-    $query = "SELECT `time_from`, `time_to` FROM timetable WHERE `day`='$day'";
+    $query = "SELECT `time_from`, `time_to` FROM timetable WHERE `day`='$day'  order by `id` asc";
     $result = mysql_query($query);
 
     $count=0;
-    $value = '';
 
 	while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
 	        foreach ($line as $col_value) {
-	            $value = $value.'-'.$col_value;
+	            if ($count == 0) {
+	            	$time_from = $col_value;
+	            } $time_to = $col_value;
 	        }
 	        $count++;
 	}
 
 	if (mysql_num_rows($result) != 0) {
-		$value = explode("-", $value);
-		$time_from = $value[0];
-		$time_to = $value[1];
 
 		$status = 'Свободно';
 		$day = date("d.m.Y");
 		$date = date("d.m.Y", strtotime($day . " +7 days"));
 
 		while ($time_from <= $time_to) {
-			$sql = 'INSERT INTO appointments (day, time, status VALUES ("'.$day.'", "'.$time_from.'", "'.$status.'")';
+			$sql = 'INSERT INTO appointments (day, time, status VALUES ("'.$date.'", "'.$time_from.'", "'.$status.'")';
 			$time_from = $time_from + 0.3
 			if ($time_from - floor($time_from) >=6) {
 				$time_from++;
