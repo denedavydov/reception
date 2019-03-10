248d0886fa8d1de42808074befe66dae2845e7d7
denedavydov <denedavydov@gmail.com>
2019-03-10 22:05:21 +0300

    Сделано модальное окно и отправка ответа пользователю

--- resources/admin/user=admin-appeals.php
+++ resources/admin/user=admin-appeals.php
@@ -89,32 +89,109 @@
 						                    $count_write_id++;
 						                }
 						                $count_write_id = 0;
-						                echo '<td><form method="POST"><button type="submit" value="'.$id.'" class="btn btn-success" name="answer"><span class="glyphicon glyphicon-share-alt"></span> Направить ответ</button></form></td>';
+						                echo '<td><form action="" method="POST" name="val"><button type="button" class="btn btn-success" data-toggle="modal" data-target="#myForm" value="'.$id.'"id="old"><span class="glyphicon glyphicon-share-alt"></span> Направить ответ</button></form></td>';
 						                $count++;
 						            echo "\t</tr>\n";
 						        }
 
-						        if (isset($_POST['answer'])) {
+						        if (isset($_POST['answer'])) { // Отправка ответа в таблицу
 						        	$id = $_POST['answer'];
-						        	$query = "UPDATE `appeals` SET `status` = 'Получен ответ' WHERE `appeals`.`id` = '$id'";
+
+						        	$theme = htmlspecialchars($_POST['theme']);
+	                				$message = htmlspecialchars($_POST['message']);
+	                				$answer = $theme.' '.$message;
+
+	                				$query = "UPDATE `appeals` SET `status` = 'Получен ответ', `answer` = '$answer' WHERE `appeals`.`id` = '$id'";
 						        	$result = mysql_query($query);
 
-						        	$num_rows = mysql_num_rows($result);
-									if ($num_rows != 0) {
-										while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
-								            foreach ($line as $col_value) {
-								                $login = $col_value;
-								                }
-								            }
-						        	}
-						        }
+						        	if(!mysql_query($query))
+					                {echo '<p class="text-danger">ОШИБКА ОТПРАВКИ ОБРАЩЕНИЯ!</p>';} 
+					                else 
+					                	{
+
+					                		$query = "SELECT `mail` FROM appeals WHERE `id` = '$id'";
+											$result = mysql_query($query);
+
+											$num_rows = mysql_num_rows($result);
+											if ($num_rows != 0) {
+												while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+										            foreach ($line as $col_value) {
+										                $mail = $col_value;
+										                }
+										            }
+										    }
+						                	//отправка письма
+									    
+										    $mail_to = $mail; // Почта получателя
+
+											$type = 'plain'; //Можно поменять на html; plain означяет: будет присылаться чистый текст.
+											$charset = 'utf-8';
+
+											include('../../templates/smtp_func.php');
+											
+											$message = $answer;
+											$subject = 'Вам был направлен ответ из "Электронная Приёмная"';
+											$mail_from = 'admin@gmail.com'; // mail школы
+											$replyto = '"Электронная приемная"';
+											$headers = "To: \"Пользователь\" <$mail_to>\r\n".
+											              "From: \"$replyto\" <$mail_from>\r\n".
+											              "Reply-To: $replyto\r\n".
+											              "Content-Type: text/$type; charset=\"$charset\"\r\n";
+											$sended = smtpmail($mail_to, $subject, $message, $headers);
+										}
+
+							    		echo '<p class="text-success">Обращение отправлено!</p>';
+							    }
+										        
 
 						        mysql_free_result($result);
 						        mysql_close($link);
-								?>
+							?>
+
+							<script type="text/javascript"> // Передача value кнопки
+									$('button#old').on('click',function(){
+									var mysuperdata = $(this).val();
+									$('button#new').val(mysuperdata);
+									});
+							</script>
 
 						</table>
 					</div>
+				</div>
+			</div>
+
+			<div class="modal fade" id="myForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				<div class="modal-dialog" role="document">
+				    <div class="modal-content">
+
+				    	<div class="modal-header">
+					      	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					          <span aria-hidden="true">&times;</span>
+					        </button>
+					        <h3 class="modal-title" id="myModalLabel">Форма для направление ответа</h3>
+				    	</div>
+
+				    	<form action="" method="POST">
+				    		<div class="modal-body">
+								<label>Заголовок ответа: </label>
+								<div class="input-group">
+									  <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-pencil"></span></span>
+									  <input type="text" name="theme" required="" maxlength="200" autocomplete="off" class="form-control" placeholder="Заголовок" aria-describedby="basic-addon1">
+								</div><br/>
+								<label>Текст ответа: </label>
+								<div class="input-group">
+									  <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-comment"></span></span>
+									  <textarea name="message" required="" rows="10" autocomplete="off" class="form-control textarea" placeholder="Текст ответа..." aria-describedby="basic-addon1"></textarea>
+								</div><br/>
+				    		</div>
+
+					    	<div class="modal-footer">
+						        <button type="button" class="btn btn-secondary" data-dismiss="modal"> Отменить </button>
+						        <button id="new" type="submit" class="btn btn-success" value="" name="answer"><span class="glyphicon glyphicon-share-alt"></span> Направить ответ </button>
+						    </div>
+				    	</form>
+
+				    </div>
 				</div>
 			</div>
 
