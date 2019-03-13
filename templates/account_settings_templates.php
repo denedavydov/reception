9262e9b256255ac7bc8087f7be411b8f628a1844
denedavydov <denedavydov@gmail.com>
2019-03-13 20:51:53 +0300

    Обновление шаблонов

--- templates/account_settings_templates.php
+++ templates/account_settings_templates.php
@@ -5,6 +5,46 @@
 	include('../../templates/session_close.php');
 ?>
 
+<?php 
+	include('../../templates/config.php');
+
+    $link = mysql_connect($db_path, $db_login, $db_password);
+	mysql_select_db($db_name) or die("Не найдена БД");
+	mysql_query('SET NAMES utf8');
+
+	if (isset($_POST['answer'])) {
+    	$login = htmlspecialchars($_SESSION['username']);
+    	$passport = htmlspecialchars($_POST['passport']);
+    	$name = htmlspecialchars($_POST['name']);
+    	$surname = htmlspecialchars($_POST['surname']);
+
+    	$query = "UPDATE `users` SET `name` = '$surname', `surname` = '$name', `passport` = '$passport' WHERE `users`.`login` = '$login'";
+    	$result = mysql_query($query);
+
+    	$query = "SELECT `password` FROM users WHERE `login` = '$login'";
+		$result = mysql_query($query);
+		$num_rows = mysql_num_rows($result);
+
+		if ($num_rows != 0) {
+			while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+        		foreach ($line as $col_value) {
+	                $pass = $col_value;
+	                }
+        		}
+        }
+
+    	if ($_POST[old_password] != none and $_POST[old_password] == $pass){
+    		if ($_POST[new_password1] == $_POST[new_password2]) {
+    			$query = "UPDATE `users` SET `password` = '$_POST[new_password1]' WHERE `users`.`login` = '$login'";
+    			$result = mysql_query($query);
+    		}else{echo "Пароли не совподают";}
+    	}else{echo "Неправильный пароль";}
+    }
+
+    mysql_free_result($result);
+    mysql_close($link);
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -26,67 +66,107 @@
 
 			<!--вывод меню-->
 			<div class="col-xs-12">
-        <ul class="nav nav-tabs">
-          <hr class="hidden-sm hidden-md hidden-lg"/>
-        	<li role="presentation"><a href="<?php $_SESSION['page'] = $_SESSION['id'].'/'.'user=id_'.$_SESSION['id']; echo 'resources/'.$_SESSION['page'].'.php' ?>" style="cursor: pointer;"><span class="glyphicon glyphicon-home"></span> <?php echo  $_SESSION['name']; ?> </a></li>
-        	<li role="presentation" class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-              <span class="glyphicon glyphicon-th-list"></span> Услуги <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu">
-              <li role="presentation"><a href="#"><span class="glyphicon glyphicon-pencil"></span> Записаться на прием</a></li>
-              <li role="presentation"><a href="<?php echo 'resources/'.$_SESSION['page'].'-appeals_'.$_SESSION['id'].'.php'; ?>"><span class="glyphicon glyphicon-file"></span> Подача обращения </a></li>
-              <li role="presentation"><a href="#"><span class="glyphicon glyphicon-open-file"></span> Запись в ШРР</a></li>
-              <li role="presentation"><a href="#"><span class="glyphicon glyphicon-open-file"></span> Запись в летний лагерь</a></li>
-            </ul>
-          </li>
-        	<li role="presentation" class="active"><a href="<?php echo 'resources/'.$_SESSION['page'].'-account_settings_'.$_SESSION['id'].'.php'; ?>"><span class="glyphicon glyphicon-user"></span> Личный кабинет</a></li>
-        </ul>
+		        <ul class="nav nav-tabs hidden-xs">
+		          <hr class="hidden-sm hidden-md hidden-lg"/>
+		        	<li role="presentation"><a href="<?php $_SESSION['page'] = $_SESSION['id'].'/'.'user=id_'.$_SESSION['id']; echo 'resources/'.$_SESSION['page'].'.php' ?>" style="cursor: pointer;"><span class="glyphicon glyphicon-home"></span> <?php echo  $_SESSION['name']; ?> </a></li>
+		        	<li role="presentation" class="dropdown">
+		            <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+		              <span class="glyphicon glyphicon-th-list"></span> Услуги <span class="caret"></span>
+		            </a>
+		            <ul class="dropdown-menu">
+		              <li role="presentation"><a href="<?php echo 'resources/'.$_SESSION['page'].'-appointments_'.$_SESSION['id'].'.php'; ?>"><span class="glyphicon glyphicon-pencil"></span> Записаться на прием</a></li>
+		              <li role="presentation"><a href="<?php echo 'resources/'.$_SESSION['page'].'-appeals_'.$_SESSION['id'].'.php'; ?>"><span class="glyphicon glyphicon-file"></span> Подача обращения </a></li>
+		              <li role="presentation"><a href="#"><span class="glyphicon glyphicon-open-file"></span> Запись в ШРР</a></li>
+		              <li role="presentation"><a href="#"><span class="glyphicon glyphicon-open-file"></span> Запись в летний лагерь</a></li>
+		            </ul>
+		          </li>
+		        	<li role="presentation" class="active"><a href="<?php echo 'resources/'.$_SESSION['page'].'-account_settings_'.$_SESSION['id'].'.php'; ?>"><span class="glyphicon glyphicon-user"></span> Личный кабинет</a></li>
+		        </ul>
 			</div>
 
-			<div class="col-xs-12">
+			<!-- Меню мобильной версии-->
+			<div class="col-xs-12 hidden-sm hidden-md hidden-lg">
+			  <br/>
+			  <nav role="navigation" class="navbar navbar-default">
+			    <!-- Логотип и мобильное меню -->
+			    <div class="navbar-header">
+			    <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			    </button>
+			      <a href="<?php $_SESSION['page'] = $_SESSION['id'].'/'.'user=id_'.$_SESSION['id']; echo 'resources/'.$_SESSION['page'].'.php' ?>" class="navbar-brand"> <span class="glyphicon glyphicon-home"></span> <?php echo  $_SESSION['name']; ?></a>
+			    </div>
+			    <!-- Навигационное меню -->
+			      <div id="navbarCollapse" class="collapse navbar-collapse">
+			        <ul class="nav navbar-nav">
+			    <!-- Выподающие меню с подпунктами -->
+			          <li class="dropdown">
+			          <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+			            <span class="glyphicon glyphicon-th-list"></span> Услуги <span class="caret"></span>
+			          </a>
+			          <ul role="menu" class="dropdown-menu">
+			            <li role="presentation"><a href="<?php echo 'resources/'.$_SESSION['page'].'-appointments_'.$_SESSION['id'].'.php'; ?>"><span class="glyphicon glyphicon-pencil"></span> Записаться на прием</a></li>
+			            <li role="presentation"><a href="<?php echo 'resources/'.$_SESSION['page'].'-appeals_'.$_SESSION['id'].'.php'; ?>"><span class="glyphicon glyphicon-file"></span> Подача обращения </a></li>
+			            <li role="presentation"><a href="#"><span class="glyphicon glyphicon-open-file"></span> Запись в ШРР</a></li>
+			            <li role="presentation"><a href="#"><span class="glyphicon glyphicon-open-file"></span> Запись в летний лагерь</a></li>
+			          </ul>
+			        </li>
+			        <li role="presentation"><a href="<?php echo 'resources/'.$_SESSION['page'].'-account_settings_'.$_SESSION['id'].'.php'; ?>"><span class="glyphicon glyphicon-user"></span> Личный кабинет</a> <hr/></li>
+			        <li><button type="submit" class="btn btn-danger btn-block" name="exit" style="margin: 5px 0px 5px 0px;"><span class="glyphicon glyphicon-log-out"></span> Выход</button></li>
+			      </ul>
+			      </div>
+			  </nav>
+			</div>
+			
+
+			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
 				<h2 class="text-primary">Настройка профиля</h2>
 			</div>
 
-			<form action="" method="POST" class="col-xs-12 col-md-5 form-horizontal">
 
-				<label>Логин пользователя:</label>
-				<div class="input-group">
-						<span class="input-group-addon" id="basic-addon1"></span>
-						<input type="text" name="name" autocomplete="off" class="form-control" placeholder="<?php echo $_SESSION['name']; ?>" aria-describedby="basic-addon1">
-				</div>
+			<form action="" method="POST" class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
 
-				<div class="input-group">
-						<span class="input-group-addon" id="basic-addon1"></span>
-						<input type="text" name="name" autocomplete="off" class="form-control" placeholder="<?php  ?>" aria-describedby="basic-addon1">
-				</div>
+				<label>Фамилия:</label>
+					<div class="input-group">
+						  <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user"></span></span>
+						  <input type="text" name="name" value="<?php echo $_SESSION['surname']; ?>" required="" autocomplete="off" class="form-control" placeholder="Ваша фамилия" aria-describedby="basic-addon1">
+					</div><br/>
+					<label>Имя Отчество:</label>
+					<div class="input-group">
+						  <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user"></span></span>
+						  <input type="text" name="surname" value="<?php echo $_SESSION['name']; ?>" required="" autocomplete="off" class="form-control" placeholder="Ваше имя и отчество" aria-describedby="basic-addon1">
+					</div><br/>
+					<label>Адрес электронной почты:</label>
+					<div class="input-group">
+						  <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-envelope"></span></span>
+						  <input value="<?php echo $_SESSION['username']; ?>" class="form-control" aria-describedby="basic-addon1" readonly>
+					</div><br/>
+					<label>Серия и номер паспорта:</label>
+					<div class="input-group">
+						  <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-book"></span></span>
+						  <input type="text" name="passport" value="<?php echo $_SESSION['passport']; ?>" autocomplete="off" minlength="9" maxlength="10" required="" class="form-control" placeholder="серия и номер Вашего паспорта (10 цифр подряд без пробелов)" aria-describedby="basic-addon1">
+					</div><br/>
+					<label>Роль:</label>
+					<div class="input-group">
+						  <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-cog"></span></span>
+						  <select class="form-control" name="status" required="">
+						  	<option><?php echo $_SESSION['status']; ?></option>
+						  	<option><?php if($_SESSION['status'] == 'Законный представитель обучающегося') {echo "Родитель обучающегося";} else { echo "Законный представитель обучающегося";} ?></option>
+						  </select>
+					</div><br/>
+					<label>Изменение пароля:</label>
+					<div class="input-group">
+						  <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-lock"></span></span>
+						  <input type="password" name="old_password" required="" minlength="6" maxlength="8" autocomplete="off" class="form-control" placeholder="Введите текущий пароль" aria-describedby="basic-addon1">
 
-				<div class="input-group">
-						<span class="input-group-addon" id="basic-addon1"></span>
-						<input type="password" name="password" maxlength="8" minlength="6" autocomplete="off" class="form-control" placeholder="Пароль пользователя" aria-describedby="basic-addon1">
-				</div>
+						  <input type="password" name="new_password1" required="" minlength="6" maxlength="8" autocomplete="off" class="form-control" placeholder="Введите новый пароль" aria-describedby="basic-addon1">
 
-				<div class="input-group">
-						<span class="input-group-addon" id="basic-addon1"></span>
-						<input type="password" name="password" maxlength="8" minlength="6" autocomplete="off" class="form-control" placeholder="Электронная почта" aria-describedby="basic-addon1">
-				</div>
+						  <input type="password" name="new_password2" required="" minlength="6" maxlength="8" autocomplete="off" class="form-control" placeholder="Повторите пароль" aria-describedby="basic-addon1">
+					</div><br/>
 
-				<div class="input-group">
-						<span class="input-group-addon" id="basic-addon1"></span>
-						<input type="password" name="password" maxlength="8" minlength="6" autocomplete="off" class="form-control" placeholder="Паспорт" aria-describedby="basic-addon1">
-				</div>
-
-				<div class="input-group">
-						<span class="input-group-addon" id="basic-addon1"></span>
-						<input type="password" name="password" maxlength="8" minlength="6" autocomplete="off" class="form-control" placeholder="ФИО ребёнка" aria-describedby="basic-addon1">
-				</div>
-
-				<div class="input-group">
-						<span class="input-group-addon" id="basic-addon1"></span>
-						<input type="password" name="password" maxlength="8" minlength="6" autocomplete="off" class="form-control" placeholder="Класс ребёнка" aria-describedby="basic-addon1">
-				</div>
-
-					<button type="submit" class="btn btn-primary" name="enter"><span class="glyphicon glyphicon-edit"></span> Сохранить изменения </button>
+					<button type="submit" class="btn btn-success" name="answer"><span class="glyphicon glyphicon-edit"></span> Сохранить изменения </button>
 			</form>
 
 			<!--подвал-->
--- templates/appeals_template.php
+++ templates/appeals_template.php
@@ -32,142 +32,138 @@
 			<?php
 				if(isset($_POST['enter'])) {
 
-						$user_id = $_SESSION['id'];
-						$mail = $_SESSION['username'];
-						$theme = htmlspecialchars($_POST['theme']);
-						$message = htmlspecialchars($_POST['message']);
-						$status = "Отправлено";
-						$date=date("d.m.");
-						$year=date("Y");
-						$time=date("h:i");
+					$user_id = $_SESSION['id'];
+					$mail = $_SESSION['username'];
+					$theme = htmlspecialchars($_POST['theme']);
+					$message = htmlspecialchars($_POST['message']);
+					$status = "Отправлено";
+					$date=date("d.m.");
+					$year=date("Y");
+					$time=date("h:i");
 
 
-						// Отправка данных в БД
+					// Отправка данных в БД
 
-						include('../../templates/config.php');
+					include('../../templates/config.php');
 
-						$link = mysql_connect($db_path, $db_login, $db_password);
-						mysql_select_db($db_name) or die("Не найдена БД");
-						mysql_query('SET NAMES utf8');
+					$link = mysql_connect($db_path, $db_login, $db_password);
+					mysql_select_db($db_name) or die("Не найдена БД");
+					mysql_query('SET NAMES utf8');
 
-						$sql = 'INSERT INTO appeals (user_id, mail, theme, message, status, date, year, time) VALUES ("'.$user_id.'", "'.$mail.'", "'.$theme.'", "'.$message.'", "'.$status.'", "'.$date.'", "'.$year.'", "'.$time.'")';                              
+					$sql = 'INSERT INTO appeals (user_id, mail, theme, message, status, date, year, time) VALUES ("'.$user_id.'", "'.$mail.'", "'.$theme.'", "'.$message.'", "'.$status.'", "'.$date.'", "'.$year.'", "'.$time.'")';                              
 
-		                if(!mysql_query($sql))
-		                {echo '<p class="text-danger">ОШИБКА ОТПРАВКИ ОБРАЩЕНИЯ!</p>';} 
-		                else 
-		                	{
-			                	//отправка письма
-						    
-							    $mail_to = $_SESSION['mail']; // Почта получателя
+	                if(!mysql_query($sql))
+	                {echo '<p class="text-danger">ОШИБКА ОТПРАВКИ ОБРАЩЕНИЯ!</p>';} 
+	                else 
+	                	{
+		                	//отправка письма
+					    
+						    $mail_to = 'admin@gmail.com'; // Почта получателя (Школы)
 
-								$type = 'plain'; //Можно поменять на html; plain означяет: будет присылаться чистый текст.
-								$charset = 'utf-8';
+							$type = 'plain'; //Можно поменять на html; plain означяет: будет присылаться чистый текст.
+							$charset = 'utf-8';
 
-								include('../../templates/smtp_func.php');
-								
-								$message = $_SESSION['username'].'   '.$_POST['message'];
-								$subject = $_POST['theme'];
-								$mail_from = $_SESSION['username'];
-								$replyto = '"Электронная приемная"';
-								$headers = "To: \"Пользователь\" <$mail_to>\r\n".
-								              "From: \"$replyto\" <$mail_from>\r\n".
-								              "Reply-To: $replyto\r\n".
-								              "Content-Type: text/$type; charset=\"$charset\"\r\n";
-								$sended = smtpmail($mail_to, $subject, $message, $headers);
-							}
+							include('../../templates/smtp_func.php');
+							
+							$message = $_SESSION['username'].'   '.$_POST['message'];
+							$subject = $_POST['theme'];
+							$mail_from = $_SESSION['username'];
+							$replyto = '"Электронная приемная"';
+							$headers = "To: \"Пользователь\" <$mail_to>\r\n".
+							              "From: \"$replyto\" <$mail_from>\r\n".
+							              "Reply-To: $replyto\r\n".
+							              "Content-Type: text/$type; charset=\"$charset\"\r\n";
+							$sended = smtpmail($mail_to, $subject, $message, $headers);
+						}
 
-							mysql_free_result($result);
-				    		mysql_close($link);
+						mysql_free_result($result);
+			    		mysql_close($link);
 
-				    		$answer='<p>Обращение отправлено!</p>';
-					}
+			    		$answer='<p>Обращение отправлено!</p>';
+				}
 			?>
 
-					<div class="col-xs-12 col-sm-12 col-md-6" style="margin-top: 15px;">
+			<div class="col-xs-12 col-sm-12 col-md-6" style="margin-top: 15px;">
 
-						<!--Форма обращения-->
-						<form action="" method="POST">
-							<label>Тема обращения: </label>
-							<div class="input-group">
-								  <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-pencil"></span></span>
-								  <input type="text" name="theme" required="" maxlength="200" autocomplete="off" class="form-control" placeholder="Тема обращения" aria-describedby="basic-addon1">
-							</div><br/>
-							<label>Текст обращения: </label>
-							<div class="input-group">
-								  <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-comment"></span></span>
-								  <textarea name="message" required="" rows="10" autocomplete="off" class="form-control textarea" placeholder="Ваше обращение" aria-describedby="basic-addon1"></textarea>
-							</div><br/>
-							<button type="submit" class="btn btn-success" name="enter"><span class="glyphicon glyphicon-log-in"></span> Отправить </button>
-						</form>
+				<!--Форма обращения-->
+				<form action="" method="POST">
+					<label>Тема обращения: </label>
+					<div class="input-group">
+						  <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-pencil"></span></span>
+						  <input type="text" name="theme" required="" maxlength="200" autocomplete="off" class="form-control" placeholder="Тема обращения" aria-describedby="basic-addon1">
+					</div><br/>
+					<label>Текст обращения: </label>
+					<div class="input-group">
+						  <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-comment"></span></span>
+						  <textarea name="message" required="" rows="10" autocomplete="off" class="form-control textarea" placeholder="Ваше обращение" aria-describedby="basic-addon1"></textarea>
+					</div><br/>
+					<button type="submit" class="btn btn-success" name="enter"><span class="glyphicon glyphicon-log-in"></span> Отправить </button>
+				</form>
 
-						<?php
-							echo $answer;
-						?>
+				<?php
+					echo $answer;
+				?>
 
+			</div>
+
+
+			<div class="col-xs-12">
+				<hr/>
+				<a onclick="$('#appeals').slideToggle('slow');" style="cursor: pointer; text-decoration: none;">
+					<h3 class="text-primary">Ваши предыдущие обращения <span class="glyphicon glyphicon-chevron-down"></span></h3>
+				</a>
+				<p class="text-success"><span class="glyphicon glyphicon-info-sign"></span> Обработанные обращения</p>
+				<div  id="appeals" style="display: none;">
+					<div class="table-responsive">
+						<table class="table table-hover table-bordered text-center">
+							<tr class="info">
+								<td colspan="2"><strong>Дата</strong></td>
+								<td><strong>Время</strong></td>
+								<td><strong>Тема</strong></td>
+								<td><strong>Текст</strong></td>
+								<td><strong>Ответ</strong></td>
+							</tr>
+
+							<?php
+
+					            include('../../templates/config.php');
+
+			                   $link = mysql_connect($db_path, $db_login, $db_password);
+								mysql_select_db($db_name) or die("Не найдена БД");
+								mysql_query('SET NAMES utf8');
+
+
+			                    $id = $_SESSION['id'];
+			                    $query = "SELECT `date`, `year`, `time`, `theme`, `message`, `answer` FROM appeals WHERE `user_id`='$id' and `status`='Получен ответ' order by `id` desc";
+			                    $result = mysql_query($query);
+
+		                        $count=0;
+
+			                    while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+			                    echo "\t<tr>\n";
+			                            foreach ($line as $col_value) {
+			                                echo "\t\t<td>$col_value</td>\n";
+			                            }
+		                                $count++;
+			                        echo "\t</tr>\n";
+			                    }
+
+			                    mysql_free_result($result);
+			                    mysql_close($link);
+		           			?>
+
+						</table>
 					</div>
-
-
-							<div class="col-xs-12">
-								<a onclick="$('#appeals').slideToggle('slow');" style="cursor: pointer; text-decoration: none;">
-									<h3 class="text-primary">Ваши предыдущие обращения <span class="
-glyphicon glyphicon-chevron-down"></span></h3>
-								</a>
-								<p class="text-success"><span class="
-glyphicon glyphicon-info-sign"></span> Обработанные обращения</p>
-								<div  id="appeals" style="display: none;">
-									<div class="table-responsive">
-										<table class="table table-hover table-bordered text-center">
-											<tr class="info">
-												<td colspan="2"><strong>Дата</strong></td>
-												<td><strong>Время</strong></td>
-												<td><strong>Тема</strong></td>
-												<td><strong>Текст</strong></td>
-											</tr>
-
-											<?php
-
-									            include('../../templates/config.php');
-
-							                   $link = mysql_connect($db_path, $db_login, $db_password);
-												mysql_select_db($db_name) or die("Не найдена БД");
-												mysql_query('SET NAMES utf8');
-
-
-							                    $id = $_SESSION['id'];
-							                    $query = "SELECT `date`, `year`, `time`, `theme`, `message` FROM appeals WHERE `user_id`='$id' and `status`='Получен ответ' order by `id` desc";
-							                    $result = mysql_query($query);
-
-						                        $count=0;
-
-							                    while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
-							                    echo "\t<tr>\n";
-							                            foreach ($line as $col_value) {
-							                                echo "\t\t<td>$col_value</td>\n";
-							                            }
-						                                $count++;
-							                        echo "\t</tr>\n";
-							                    }
-
-							                    mysql_free_result($result);
-							                    mysql_close($link);
-						           			?>
-
-										</table>
-									</div>
-								</div>
-							</div>
-
-
-
-						<!--подвал-->
-						<div class="col-xs-12 text-center">
-							<?php include('../../templates/footer.php'); ?>
-						</div>
-
 				</div>
 			</div>
 
-	
+
+
+			<!--подвал-->
+			<div class="col-xs-12 text-center">
+				<?php include('../../templates/footer.php'); ?>
+			</div>
+
 		</div>
 	</div>
 
